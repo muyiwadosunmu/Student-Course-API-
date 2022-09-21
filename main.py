18e3df22b46d9f791e9acf5b3ebfcfcd68266150
Muyiwa <oluwamuyiwadosunmu@gmail.com>
2022-09-21 17:27:45 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, Path, Query
+from pydantic import BaseModel
+from typing import Optional, List
+
+app = FastAPI()
+
+users = []
+
+
+class User(BaseModel):
+    email: str
+    is_active: bool
+    bio:Optional[str]
+    age: int
+
+
+@app.get("/users", response_model=List[User])
+async def get_user():
+    return users
+
+
+@app.post("/user")
+async def create_user(user: User):
+    users.append(user)
+    return users
+
+@app.get("/users/{id}")
+async def get_user(
+    id:int = Path(..., description="The ID of the user you want to retrieve"),
+    is_active:str = Query(None,max_length=5)
+):
+    return {"users": users[id], "query":q}
