a751cca8c1129768938a6069c1468b54b775642f
Muyiwa <oluwamuyiwadosunmu@gmail.com>
2022-09-30 02:27:53 +0100

    Every endpoints now working

--- api/courses.py
+++ api/courses.py
@@ -17,14 +17,14 @@ async def read_courses(db: Session = Depends(get_db)):
 
 @router.post("/courses", response_model=Course)
 async def create_new_course(course: CourseCreate, db: Session = Depends(get_db)):
-    return create_course
+    return create_course(db=db, course=course)
 
 
 @router.get("/courses/{course_id}")
-async def read_course(course_id:int, db:Session=Depends(get_db)):
+async def read_course(course_id: int, db: Session = Depends(get_db)):
     db_course = get_course(db=db, course_id=course_id)
     if db_course is None:
-        raise HTTPException(status_code=404, details="Course not found")
+        raise HTTPException(status_code=404, detail="Course not found")
     return db_course
 
 
